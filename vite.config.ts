b4bfefb4849d8e497436afefe69ac865e7adbaf2
sandaruDevr <sadaludev@gmail.com>
2025-04-25 02:35:23 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  base: '/app/', // 👈 IMPORTANT: All assets will load from /app/
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // (default is dist, can be skipped if unchanged)
+  },
 })
+
